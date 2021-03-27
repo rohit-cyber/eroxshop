add72a904d872efba91a914cc8c8db7fad9347a3
rohit-cyber <rohitjune15@gmail.com>
2021-03-27 18:37:28 +0530

    created admin

--- eroxshop/app/admin.py
+++ eroxshop/app/admin.py
@@ -1,12 +1,23 @@
-# from django.contrib import admin
-# from .models import Customer,Product,OrderPlaced,Cart
+from django.contrib import admin
+from .models import Customer,Product,OrderPlaced,Cart
 
-# # Register your models here.
+# Register your models here.
 
-# class CustomerAdmin(admin.ModelAdmin):
-#     list_display=['id','user','name','locality','city','zipcode','state']
+class CustomerAdmin(admin.ModelAdmin):
+    list_display=['id','user','name','locality','city','zipcode','state']
 
-# class ProductAdmin(admin.ModelAdmin):
-#     list_display=[]
+class ProductAdmin(admin.ModelAdmin):
+    list_display=['id','title','selling_price','discounted_price','description','brand','category','product_image']
+
+class CartAdmin(admin.ModelAdmin):
+    list_display=['user','product','quantity']
+
+class OrderPlacedAdmin(admin.ModelAdmin):
+    list_display=['user','customer','product','quantity','ordered_date','status']
+
+admin.site.register(Customer,CustomerAdmin)
+admin.site.register(Product,ProductAdmin)
+admin.site.register(Cart,CartAdmin)
+admin.site.register(OrderPlaced,OrderPlacedAdmin)
 
 
