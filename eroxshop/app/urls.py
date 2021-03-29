8634758c5f9b209a736421336d82d12593a22c2e
rohit-cyber <rohitjune15@gmail.com>
2021-03-29 15:55:29 +0530

    setting mobile page

--- eroxshop/app/urls.py
+++ eroxshop/app/urls.py
@@ -3,15 +3,20 @@ from django.conf import settings
 from django.conf.urls.static import static
 from app import views
 urlpatterns = [
-    path('', views.home),
-    path('product-detail/', views.product_detail, name='product-detail'),
+    path('', views.ProductView.as_view(),name='product'),
+
+    path('product-detail/<int:pk>', views.ProductDetail.as_view(), name='product-detail'),
+
+     path('mobile/', views.mobile, name='mobile'),
+     path('mobile/<str:data>',views.mobile, name='mobiledata'),
+
     path('cart/', views.add_to_cart, name='add-to-cart'),
     path('buy/', views.buy_now, name='buy-now'),
     path('profile/', views.profile, name='profile'),
     path('address/', views.address, name='address'),
     path('orders/', views.orders, name='orders'),
     path('changepassword/', views.change_password, name='changepassword'),
-    path('mobile/', views.mobile, name='mobile'),
+   
     path('login/', views.login, name='login'),
     path('registration/', views.customerregistration, name='customerregistration'),
     path('checkout/', views.checkout, name='checkout'),
--- eroxshop/app/views.py
+++ eroxshop/app/views.py
@@ -1,10 +1,36 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Product,Customer,Cart,OrderPlaced
 
-def home(request):
- return render(request, 'app/home.html')
+# def home(request):
+#  return render(request, 'app/home.html')
 
-def product_detail(request):
- return render(request, 'app/productdetail.html')
+class ProductView(View):
+    def get(self,request):
+        topwear=Product.objects.filter(category='TW')
+        bottomwear=Product.objects.filter(category='BW')
+        mobile=Product.objects.filter(category='M')
+        dictt={'topwear':topwear,'bottomwear':bottomwear,'mobile':mobile}
+        return render(request,'app/home.html',context=dictt)
+
+
+class ProductDetail(View):
+    def get(self,request,pk):
+        product=Product.objects.get(id=pk)
+        return render(request,'app/productdetail.html',{'product':product})
+
+def mobile(request,data=None):
+    if data == None:
+        product=Product.objects.filter(category='M')
+    elif data=='Samsung':
+        product=Product.objects.filter(category='M').filter(brand='samsung')
+    elif data=='Apple':
+        product=Product.objects.filter(category='M').filter(brand='apple')
+    elif data=='Above':
+        product=Product.objects.filter(category='M').filter(discounted_price__gt=500)
+    elif data=='Below':
+        product=Product.objects.filter(category='M').filter(discounted_price__lt=500)
+    return render(request, 'app/mobile.html',{'product':product})
 
 def add_to_cart(request):
  return render(request, 'app/addtocart.html')
@@ -24,8 +50,7 @@ def orders(request):
 def change_password(request):
  return render(request, 'app/changepassword.html')
 
-def mobile(request):
- return render(request, 'app/mobile.html')
+
 
 def login(request):
  return render(request, 'app/login.html')
